90490280b3e9b0de64bc3500cfaa5ac10ae1d2d5
Filth <codeinfilth@gmail.com>
2023-01-24 17:25:42 -0800

    added file changes

---
+++ system/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Newuser(models.Model):
+	Username=models.CharField(max_length=150)
+	Email=models.CharField(max_length=150)
+	Pwd=models.CharField(max_length=150)
+	Age=models.IntegerField()
+	Gender=models.CharField(max_length=1)
